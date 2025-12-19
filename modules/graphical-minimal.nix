e507baee40666aea3c8cbf920824ebf51db3f1c1
Asher <146795551+ALH477@users.noreply.github.com>
2025-12-19 09:01:21 -0800

    Create graphical-minimal.nix

---
+++ modules/graphical-minimal.nix
@@ -0,0 +1,46 @@
+# modules/graphical-minimal.nix
+{ pkgs, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+
+    displayManager.lightdm = {
+      enable = true;
+      background = "#111111";
+      greeters.slick = {
+        enable = true;
+        theme.name = "Adwaita-dark";
+      };
+    };
+
+    displayManager.autoLogin = {
+      enable = true;
+      user = "nixos";
+    };
+
+    windowManager.dwm.enable = true;
+    layout = "us";
+  };
+
+  environment.systemPackages = with pkgs; [
+    firefox
+    curl
+    bash
+    dwm
+    dmenu
+    st
+    xterm
+    feh
+  ];
+
+  users.users.nixos = {
+    isNormalUser = true;
+    description = "Live System User";
+    extraGroups = [ "wheel" "podman" "networkmanager" ];
+    initialPassword = "nixos";
+    linger = true;
+  };
+
+  networking.hostName = "offline-assistant-minimal";
+}
