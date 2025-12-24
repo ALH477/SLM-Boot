7e522d1649c4abf0b2b422246c2e6993c0f75850
Asher <146795551+ALH477@users.noreply.github.com>
2025-12-24 11:40:22 -0800

    Update preload.nix

--- modules/preload.nix
+++ modules/preload.nix
@@ -1,50 +1,9 @@
 # modules/preload.nix
-{ pkgs, config, ... }:
+{ config, pkgs, ... }:
 
-let
-  preloadedOllamaModels = pkgs.stdenv.mkDerivation {
-    name = "ollama-preloaded-models";
-    src = ../ollama-preloaded-models;
-    dontUnpack = true;
-    installPhase = ''
-      mkdir -p $out
-      cp -r $src/* $out/
-    '';
-  };
-
-  preloadedOpenWebUIImage = pkgs.stdenv.mkDerivation {
-    name = "open-webui-image";
-    src = ../open-webui-ollama.tar;
-    dontUnpack = true;
-    installPhase = ''
-      mkdir -p $out
-      cp $src $out/image.tar
-    '';
-  };
-
-  preloadedOllamaImage = pkgs.stdenv.mkDerivation {
-    name = "ollama-image";
-    src = ../ollama-cpu.tar;
-    dontUnpack = true;
-    installPhase = ''
-      mkdir -p $out
-      cp $src $out/image.tar
-    '';
-  };
-in
 {
-  system.activationScripts.preload-ollama-models.text = ''
-    OLLAMA_DIR="/var/lib/ollama/.ollama"
-    mkdir -p "$OLLAMA_DIR"
-    if [ -z "$(ls -A "$OLLAMA_DIR")" ]; then
-      echo "Pre-loading Ollama models..."
-      cp -r ${preloadedOllamaModels}/* "$OLLAMA_DIR"/
-      chown -R nixos:users "$OLLAMA_DIR"
-      chmod -R 750 "$OLLAMA_DIR"
-    fi
-  '';
+  # Preload services removed – not needed for basic offline setup
+  # (Ollama runs natively, Open WebUI can be Nix-packaged later if needed)
 
-  _module.args = {
-    inherit preloadedOllamaModels preloadedOpenWebUIImage preloadedOllamaImage;
-  };
+  environment.systemPackages = [ pkgs.podman ];  # keep Podman if you plan containers later
 }
